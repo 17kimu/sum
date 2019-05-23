9a1058f970fd4441265772752a7eea512b51627a
sayaka <sayaka0530kimura@yahoo.co.jp>
2019-05-23 23:36:56 +0900

    profile3

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'],function(){
-    Route::get('news/create','Admin\NewsController@add');
+    Route::get('admin/profile/create','Admin\ProfileController@add');
 });
 
 Route::get('http://XXXXXX.jp/XXX', 'Admin\AAAContrller@bbb');
