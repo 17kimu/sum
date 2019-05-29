bc3a2910c81a5d315a2f28310c74b2f193eb8b57
sayaka <sayaka0530kimura@yahoo.co.jp>
2019-05-29 21:31:06 +0900

    news

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,14 +1,13 @@
- <!DOCTYPE html>
+@extends('layouts.profile')
 
- <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title','Myプロフィール')
 
-        <title>MyNews</title>
-    </head>
-    <body>
-       <h1>Myニュース作成画面</h1>
-    </body>
-</html>
+@section('content')
+     <div class ="container">
+        <div class="row">
+           <div class="col-md-8 mx-auto">
+               <h2>Myプロフィール</h2>
+           </div>
+        </div>
+     </div>
+@endsection
